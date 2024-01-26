dfc675bbb2b1382ced15577e494f63a61eee0120
monciego <jerichobantiquete@gmail.com>
2024-01-26 23:21:20 +0800

    factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
